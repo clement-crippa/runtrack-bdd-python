53ed070bd2b658431d6d2155023ca85283a18bf7
clement-crippa <clement.crippa@laplateforme.io>
2023-03-14 19:30:29 +0100

    Rendu des jobs 7-8

---
+++ jour02/job07.py
@@ -0,0 +1,55 @@
+import mysql.connector
+
+class CRUD:
+    def __init__(self, host, user, password, database):
+        self.bdd = mysql.connector.connect(host=host, user=user, password=password, database=database)
+
+    def Afficher_Employes(self):
+        cursor = self.bdd.cursor()
+        cursor.execute("select * from employes;")
+        for employes in cursor:
+            print(employes)
+
+    def Afficher_Employes3K(self):
+        cursor = self.bdd.cursor()
+        cursor.execute("select * from employes where salaire>3000;")
+        for salaire in cursor:
+            print("Voici les employés ayant un salaire supérieur a 3000€:", salaire[1], salaire[2], salaire[3], "€")
+
+    def Afficher_Employes_Services(self):
+        cursor = self.bdd.cursor()
+        cursor.execute("select e.*, s.nom from employes e inner join services s on e.id_service = s.id;")
+        for x in cursor:
+            print(x)
+
+    def Supprimer_Employes(self, id):
+        cursor = self.bdd.cursor()
+        cursor.execute("delete from employes where id=(%s);", (id,))
+
+    def Ajouter_Employes(self, nom, prenom, salaire, id_service):
+        cursor = self.bdd.cursor()
+        cursor.execute("insert into employes (nom,prenom,salaire,id_service) values (%s, %s, %s, %s);",
+                       (nom, prenom, salaire, id_service))
+
+    def Modifier_Salaire(self, id, salaire):
+        cursor = self.bdd.cursor()
+        cursor.execute("update employes set salaire=(%s) where id=(%s);", (salaire, id,))
+
+    def Fermer_Session(self):
+        cursor= self.bdd.cursor()
+        cursor.close()
+
+
+crud = CRUD("localhost", "root", "rootmdp", "laplateforme")
+crud.Afficher_Employes()
+crud.Afficher_Employes3K()
+print("---------------------")
+crud.Ajouter_Employes("Bobbyne", "Bobby", 2500, 1)
+crud.Afficher_Employes()
+print("---------------------------")
+crud.Afficher_Employes_Services()
+print("------------------------")
+crud.Modifier_Salaire(23, 3500)
+crud.Afficher_Employes3K()
+
+crud.Fermer_Session()
---
+++ jour02/job08.py
@@ -0,0 +1,60 @@
+import mysql.connector
+
+class Zoo:
+    def __init__(self, host, user, password, database):
+        self.bdd = mysql.connector.connect(host=host, user=user, password=password, database=database)
+
+    def Afficher_Animal(self):
+        cursor = self.bdd.cursor()
+        cursor.execute("select * from animal;")
+        for animal in cursor:
+            print(animal)
+
+
+    def Afficher_Animal_Cage(self):
+        cursor = self.bdd.cursor()
+        cursor.execute("select a.*, c.nom from animal a inner join cage c on a.id_cage = c.id;")
+        for x in cursor:
+            print(x)
+    def Superficie(self):
+        cursor=self.bdd.cursor()
+        cursor.execute("select sum(superficie) from cage;")
+        for superficie in cursor:
+            print("La superficie total de toute les cages est de",superficie[0],"m²")
+    def Supprimer_Animal(self, id):
+        cursor = self.bdd.cursor()
+        cursor.execute("delete from animal where id=(%s);", (id,))
+    def Ajouter_Cage(self,superficie, id_cage,capacite):
+        cursor = self.bdd.cursor()
+        cursor.execute("insert into cage (superficie,id_cage,capacite) values (%s, %s, %s);",(superficie,id_cage,capacite))
+
+    def Ajouter_Animal(self, nom, race, id_cage, naissance,origine):
+        cursor = self.bdd.cursor()
+        cursor.execute("insert into animal (nom,race,id_cage,naissance,origine) values (%s, %s, %s, %s,%s);",(nom, race, id_cage, naissance,origine))
+
+    def Deplacer_Animal(self, id_cage, id):
+        cursor = self.bdd.cursor()
+        cursor.execute("update animal set id_cage=(%s) where id=(%s);", (id_cage, id,))
+
+    def Fermer_Session(self):
+        cursor= self.bdd.cursor()
+        cursor.close()
+
+
+zoo = Zoo("localhost", "root", "rootmdp", "laplateforme")
+zoo.Ajouter_Animal("Lion","Sauvage",1,1998,"Australie")
+zoo.Ajouter_Animal("Renard","Sauvage",1,1999,"Australie")
+zoo.Ajouter_Animal("Singe","Sauvage",2,2000,"France")
+print("-----------------")
+zoo.Afficher_Animal()
+print("-------------------")
+zoo.Ajouter_Cage(1000,1,4)
+zoo.Ajouter_Cage(200,2,2)
+zoo.Superficie()
+print("------------------")
+zoo.Afficher_Animal_Cage()
+print("------------------")
+zoo.Deplacer_Animal(2,1)
+zoo.Afficher_Animal_Cage()
+print("-----------------")
+zoo.Fermer_Session()
